78df1a1d794d5f0618a27062d4f2be89cf708159
Hanica Cabato <cabato.hanica@gmail.com>
2021-11-03 16:55:15 +0800

    add cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 import userRouter from './routes/user';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
 app.use('/user', userRouter);
 
 export default app;
